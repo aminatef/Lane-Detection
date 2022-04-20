894ee664a9211fd0072eb67832b95519ccbefc15
aminatef <48862934+aminatef@users.noreply.github.com>
2022-04-20 17:56:45 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,17 +1,18 @@
-while getopts 'df:v' flag; do
+while getopts 'dpf:v' flag; do
   case "${flag}" in
     d) d_flag='true' ;;
+    p) d_flag='false' ;;
     f) files="${OPTARG}" ;;
-    *) print_usage
-       exit 1 ;;
   esac
 done
 echo $files
 echo $d_flag
-if d_flag="true" 
+
+if $d_flag = "true" 
 then
   python main.py $files -d
 else
   python main.py $files -p
 fi
+
 
